3fd290aab794c2aa64668367d0a3ff3b285c8d22
Айдил Умаров <aidildev@tuta.io>
2020-10-16 21:21:30 +0600

    Implemented base ViewModel

---
+++ ToDoOrNotToDo/ToDoOrNotToDo/ToDoOrNotToDo/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace ToDoOrNotToDo.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public INavigation Navigation { get; set; }
+
+        public void RaisePropertyChanged(params string[] propertyNames)
+        {
+            foreach (var propertyName in propertyNames)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
