afaf95d8829c82a5a9de7de832a24f5cd7706ed8
Айдил Умаров <aidildev@tuta.io>
2020-10-16 17:13:45 +0600

    Added TodoItem model

---
+++ ToDoOrNotToDo/ToDoOrNotToDo/ToDoOrNotToDo/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoOrNotToDo.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Completed { get; set; }
+
+        public DateTimeOffset Due { get; set; }
+
+    }
+}
