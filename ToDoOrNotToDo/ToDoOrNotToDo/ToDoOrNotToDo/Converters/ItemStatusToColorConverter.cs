1b353a4435f65d797bbd3f02002eac3dea0b0984
aidilumarov <aidildev@tuta.io>
2020-10-18 21:30:47 +0600

    Value converter to convert from item status to background color

---
+++ ToDoOrNotToDo/ToDoOrNotToDo/ToDoOrNotToDo/Converters/ItemStatusToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDoOrNotToDo.Converters
+{
+    public class ItemStatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value
+                ? (Color)Application.Current.Resources["ItemCompletedColor"]
+                : (Color)Application.Current.Resources["ItemActiveColor"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
