03e88d0679fd050792e004c9c80001bc13ffae52
Айдил Умаров <aidildev@tuta.io>
2020-10-20 17:47:47 +0600

    Light theme

---
+++ ToDoOrNotToDo/ToDoOrNotToDo/ToDoOrNotToDo/Styles/Light.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoOrNotToDo.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Light : ResourceDictionary
+    {
+        public Light() : base()
+        {
+        }
+    }
+}
